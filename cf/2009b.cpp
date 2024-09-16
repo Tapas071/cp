6ff30f6dca4c9d1fe92b1351b10e74197ab9d519
Tapas071 <tapas710mondal@gmail.com>
2024-09-16 16:56:39 +0530

    adding one more easy cf question

---
+++ cf/2009b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int  a,b;
+    cin>>a>>b;
+    cout<<6-a-b<<endl;
+
+
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*
+ problem link :https://codeforces.com/problemset/problem/2009/B
+
+ Status : unsolved
+
+
+*/
