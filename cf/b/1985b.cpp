a2b926d919a9378584dd45a091452900478c18f2
tapas <tapas710mondal@gmail.com>
2024-09-26 12:16:43 +0530

    adding 1985b.cpp

---
+++ cf/b/1985b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n , ans;
+    cin>>n;
+	cout << (n == 3 ? 3 : 2) << endl;
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*
+ problem link :https://codeforces.com/problemset/problem/2002/A
+
+ Status : unsolved
+
+
+*/
+
