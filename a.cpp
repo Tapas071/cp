bc05dc562e219d7ef79ebf2eefaf4cbdebd86510
Tapas Mondal <tapas710mondal@gmail.com>
2025-01-26 08:28:19 +0530

    lc some problem

--- a.cpp
+++ a.cpp
@@ -1,30 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-void solve()
-{
-    int n,k, sum =0;
-    cin>>n>>k;
-    for( int i =0; i<n ; i++){
-        int x ;
-        cin>>x;
-        sum+= x;
-    }
-    if( sum%k==0)cout<<sum/k<<endl;
-    else cout<<(sum/k)+1<<endl;
 
-}
-
-int main()
+class Solution
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    int t = 1;
-    cin >> t;
-    while (t--)
+public:
+    vector<int> countMentions(int numberOfUsers, vector<vector<string>> &events)
     {
-        solve();
+        set<int> current;
+    }
+};
+
+    int
+    main()
+{
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    int t = 1;
+    // cin >> t;
+    // while (t--)
+    {
+        // solve();
     }
     return 0;
 }
