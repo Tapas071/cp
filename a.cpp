1812b2cb6461cd104ede4aa1df93961dce5f6ee9
Tapas Mondal <tapas710mondal@gmail.com>
2026-02-01 13:28:33 +0530

    adding 1726a cf question from tle eliminator

--- a.cpp
+++ a.cpp
@@ -2,28 +2,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 // powershell  terminal cmd to run the code:  g++ a.cpp -o a.exe ; .\a.exe
-// powershell cmd to push into git:  git status; git add .; git commit -m "adding a few cf question"; git push
+// powershell cmd to push into git:  git status; git add .; git commit -m "adding 1726a cf question"; git push
 
 void solve()
 {
  int n;
  cin>>n;
  vector<int> nums(n);
- for( int i =0;i< n; i++){
+ for( int i =0; i< n; i++){
     cin>>nums[i];
-    if(nums[i]==1)nums[i]++;
  }
- 
- for( int i =0; i<n-1;i++){
-    if( nums[i+1]%nums[i] == 0){
-        nums[i+1]++;
-    }
+ int ans =0;
+ for( int i =0; i< n-1; i++){
+    ans = max( ans, nums[n-1]-nums[i]);
  }
-for( int i =0; i< n; i++)
-{
-    cout<<nums[i]<<" ";
-}
-cout<<endl;
+ for (int i = 1; i < n ; i++)
+ {
+     ans = max(ans, nums[i] - nums[0]);
+ }
+ for( int i=1;i< n;i++){
+    ans = max( ans, (nums[i-1]- nums[i]));
+ }
+ cout<<ans<<endl;
  return;
 }
 
@@ -44,29 +44,5 @@ int main()
 }
 
 /*
-1828b.cpp
-
-3 1 2
-
-1 3 2
-1 2 3
-k =1
-
-3 4 1 2
-
-1 4 3 2
-1 2 3 4
-k = 2
-
-4 2 6 7 5 3 1
-
-1 6 7 4 9 2 3 8 5
-
-1 5 3 4 2 6
-
-
-
-
-
 
 */
---
+++ cf/a/1726a.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+// powershell  terminal cmd to run the code:  g++ a.cpp -o a.exe ; .\a.exe
+// powershell cmd to push into git:  git status; git add .; git commit -m "adding a few cf question"; git push
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans = max(ans, nums[n - 1] - nums[i]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        ans = max(ans, nums[i] - nums[0]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        ans = max(ans, (nums[i - 1] - nums[i]));
+    }
+    cout << ans << endl;
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*
+
+*/
